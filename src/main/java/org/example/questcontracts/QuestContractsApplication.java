7db4c7819444ba8dae50e1eb709a0d6f6097dcde
Xx-Aiser-xX <stas.plot@mail.ru>
2024-12-21 19:27:47 +0300

    init

---
+++ src/main/java/org/example/questcontracts/QuestContractsApplication.java
@@ -0,0 +1,13 @@
+package org.example.questcontracts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestContractsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestContractsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/questcontracts/QuestContractsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.questcontracts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestContractsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
